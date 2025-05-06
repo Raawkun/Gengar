49904942702054831d0eeac03a0ccad4fe326dbc
Rawkun <badexploits@gmail.com>
2025-05-06 13:59:00 +0200

    module.py aktualisieren

--- cogs/module.py
+++ cogs/module.py
@@ -192,9 +192,7 @@ class Modules(commands.Cog):
             elif "won the battle" in message.content:
                 coins = message.content.lower().split(" pokecoins")[0]
                 coins = coins.split(" ")
-                print(coins)
-                gth = len(coins)
-                print(gth)
+                gth = len(coins)-1
                 coins = int(coins[gth].replace(",",""))
                 self.db.execute(f"UPDATE DailyStats SET BattleWon = BattleWon+1,CoinBattle = CoinBattle + {coins} WHERE Date = '{date}'")
                 self.db.commit()
