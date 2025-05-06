8fb23e5b594a687474477f7b4ede3b919ed85d34
Rawkun <badexploits@gmail.com>
2025-05-06 06:03:44 +0200

    module.py aktualisieren

--- cogs/module.py
+++ cogs/module.py
@@ -142,7 +142,7 @@ class Modules(commands.Cog):
             channel = self.client.get_channel(channel)
             guild = channel.guild
             coin = "<:pokecoin:835054000063381516>"
-            embed = disnake.Embed(title="Daily Server Stats",description=f"The daily server stats for {guild.name}.",timestamp=int(now.timestamp()),color=disnake.Color.blurple)
+            embed = disnake.Embed(title="Daily Server Stats",description=f"The daily server stats for {guild.name}.",color=disnake.Color.blurple)
             row = self.db.execute(f"SELECT * FROM DailyStats WHERE Date = '{date}'")
             
             embed.add_field(name="**Coins**",value=f"Total: {(row[1]+row[2]+row[3]+row[4]+row[5]):,} {coin}\nFrom Catches: {row[1]:,} {coin}\nFrom Battles: {row[2]:,} {coin}\nFrom Market: {row[3]:,} {coin}\nFrom Releasing: {row[4]:,} {coin}\nFrom World Boss: {row[5]:,} {coin}",inline=True)
