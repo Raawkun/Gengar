6679cb5e086aca57915367f0f3595e95a750e1b7
Rawkun <badexploits@gmail.com>
2025-05-12 21:06:57 +0200

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -164,7 +164,7 @@ class Coms(commands.Cog):
         except Exception as e:
             await ctx.send(f"Error reloading `{extension}`.\n```{git_output}```")
         
-    @commands.check(Basic_checker.check_management())
+    @commands.check(Basic_checker.check_management)
     @commands.command(aliases=[("daily")])
     async def dailystats(self, ctx):
         try:
