a9758f5fc0a233ede14ddd52aa911d9dd8d32676
Rawkun <badexploits@gmail.com>
2025-05-16 17:27:53 +0200

    WB RareSpawn

--- cogs/listener.py
+++ cogs/listener.py
@@ -404,6 +404,8 @@ class Listener(commands.Cog):
                 asyncio.create_task(Modules.dailycheck(self,message))
             if "here are your rewards for the " in message.content.lower():
                 asyncio.create_task(Modules.dailycheck(self, message))
+                if "you obtained a" in message.content.lower():
+                    asyncio.create_task(Modules.rare_spawn(self, message))
             if "used a code to claim" in message.content:
                 monname = message.content.split("**")[1]
                 print(monname)
