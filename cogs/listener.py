6a723f37dc8c2ca4ccea9fcddee9edbe52e245d8
Rawkun <badexploits@gmail.com>
2025-05-18 13:37:38 +0200

    Update listener.py

--- cogs/listener.py
+++ cogs/listener.py
@@ -85,7 +85,7 @@ class Listener(commands.Cog):
                 name = 'Dawn'
                 desc = f"<@&1338742032809590786>\nIts time! Go get your stones!\nCurrent Stone: <:dawn_stone:1339193944575053865> Dawn Stone"
             wait_sec = (next-now).total_seconds()
-            self.db.execute(f"INSERT INTO Timers VALUES ('{name}'),1,827306503866155008,{int(next.timestamp())}")
+            self.db.execute(f"INSERT INTO Timers VALUES ('{name}',1,827306503866155008,{int(next.timestamp())})")
             self.db.commit()
             await asyncio.sleep(wait_sec)
             self.db.execute(f"DELETE FROM Timers WHERE '{name}' = Name")
