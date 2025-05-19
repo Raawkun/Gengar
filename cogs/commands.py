5bce82cd004c6a482252dcb5f1c58b427572c831
Rawkun <badexploits@gmail.com>
2025-05-19 12:12:26 +0200

    commands.py aktualisieren

--- cogs/commands.py
+++ cogs/commands.py
@@ -149,9 +149,9 @@ class Coms(commands.Cog):
                             emb.add_field(name=f"Biggest Catch:",value=f"{row[2]/100}m")
                             emb.add_field(name=f"Smallest Catch:",value=f"{row[3]/100}m")
                             emb.set_thumbnail(url=pic)
-                            jk = f"You're currently placed on #{i+1} in the Event Leaderboard, with a catch of {row[2]/100}m.\nYour smallest catch is {row[3]/100}m."
-                            jk = ""
-                            await ctx.reply(embed=emb)
+                            #jk = f"You're currently placed on #{i+1} in the Event Leaderboard, with a catch of {row[2]/100}m.\nYour smallest catch is {row[3]/100}m."
+                            jk += f"The event will run until <t:{timeing}:f>"
+                            await ctx.reply(jk,embed=emb)
                             return
                         except Exception as e:
                             print(e)
