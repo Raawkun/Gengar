747cd415a8ce63c953487970410d6b7aec0767f7
Rawkun <badexploits@gmail.com>
2025-05-19 11:29:47 +0200

    commands.py aktualisieren

--- cogs/commands.py
+++ cogs/commands.py
@@ -145,8 +145,8 @@ class Coms(commands.Cog):
                         emb = await disnake.Embed(
                         title=f"Karp Fishing Event", color=disnake.Color.cyan())
                         emb.add_field(name=f"Current Place:", value=f"{i+1}")
-                        emb.add_field(name=f"Biggest Catch:",value=(f"{row[2]/100}m")
-                        emb.add_field(name=f"Smallest Catch:",value=f"{row[3]/100}m")
+                        emb.add_field(name=f"Biggest Catch:",value=(f"{row[2]/100}m"))
+                        emb.add_field(name=f"Smallest Catch:",value=f"{row[3]/100}m"))
                         embed.set_thumbnail(url=pic)
                         jk = f"You're currently placed on #{i+1} in the Event Leaderboard, with a catch of {row[2]/100}m.\nYour smallest catch is {row[3]/100}m."
                         jk = ""
