c362d425ec296c286e8fa01ba52c7cb64240951a
Rawkun <badexploits@gmail.com>
2025-05-29 21:41:58 +0200

    commands.py aktualisieren

--- cogs/commands.py
+++ cogs/commands.py
@@ -185,14 +185,14 @@ class Coms(commands.Cog):
                     if row[0] == ctx.author.id:
                         try:
                             pic = check[5]
-                            col = type_colors[f'{pic}']
+                            col = type_colors[pic]
                             pic = pic.split("type:")[1]
                             pic = pic.split(">")[0]
                             pic = f'https://cdn.discordapp.com/emojis/{pic}.webp?size=240'
                             emb = disnake.Embed(
-                            title=f"Type Hunt Event", color=disnake.Color.teal())
+                            title=f"Type Hunt Event", color=type_colors[check[5])
                             emb.add_field(name=f"Current Place:", value=f"{i+1}")
-                            emb.add_field(name=f"Points",value=f"{row[2]/100}m")
+                            emb.add_field(name=f"Points",value=row[2])
                             emb.add_field(name="Total Catches:", value=row[1])
                             emb.set_thumbnail(url=pic)
                             #jk = f"You're currently placed on #{i+1} in the Event Leaderboard, with a catch of {row[2]/100}m.\nYour smallest catch is {row[3]/100}m."
