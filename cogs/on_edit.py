fc2cd1e687ddbd87a2aea2e4c5f903964d489fc6
Rawkun <badexploits@gmail.com>
2025-05-29 18:39:08 +0200

    on_edit.py aktualisieren

--- cogs/on_edit.py
+++ cogs/on_edit.py
@@ -104,7 +104,7 @@ class On_Edit(commands.Cog):
                                     asyncio.create_task(Modules.averagecoins(self,after))
                                 if data[0] == 129:
                                     asyncio.create_task(Modules.fisheventcheck(self, after,sender))
-                                if (data[2] == Modules.hunted_type) or (data[3] == Modules.hunted_type):
+                                if Modules.hunted_type == data[2] or data[3]:
                                     print(f"{data[2]} {data[3]}")
                                     asyncio.create_task(Modules.eventchecker(self, after,sender))
                                 if "_fossil" in _embed.description:
