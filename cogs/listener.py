8b4674e44bdb7a970951e9de781d602e82f8b4b7
Rawkun <badexploits@gmail.com>
2025-09-02 16:22:16 +0200

    listener.py aktualisieren

--- cogs/listener.py
+++ cogs/listener.py
@@ -732,7 +732,7 @@ class Listener(commands.Cog):
                                     await message.channel.send(desc)
 
                     
-                        await asyncio.sleep(9)
+                        await asyncio.sleep(9.2)
                         datarem = self.db.execute(f'SELECT * FROM Toggle WHERE User_ID = {sender.id}')
                         datarem = datarem.fetchone()
                         if datarem[10] == 1:
