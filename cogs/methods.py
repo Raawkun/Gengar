bfb5f87d2fe3acd6a0f725aa8b6f56adbebd9b80
Rawkun <badexploits@gmail.com>
2026-01-04 16:16:55 +0100

    methods.py aktualisieren

--- cogs/methods.py
+++ cogs/methods.py
@@ -1,5 +1,10 @@
 import disnake
-from disnake.ext import Commands
+from disnake.ext import commands
 import math
 
-class Methods
+class Methods(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.db = connect("database.db")
+
