bf938aaafd8592b5974749df36d9c97c0332133b
Rawkun <badexploits@gmail.com>
2026-01-04 16:30:58 +0100

    methods.py aktualisieren

--- cogs/methods.py
+++ cogs/methods.py
@@ -19,8 +19,14 @@ class Methods(commands.Cog):
         spdef = 
         speed = 
         image = embed.image.url()
-        dex = 
-        
+        dex = self.db.execute(f"SELECT * FROM Dex Where Img_url = {image}")
+        dex = dex.fetchone()
+        baseatk = dex[5]
+        basedef=dex[6]
+        basehp=dex[4]
+        basespatk=dex[7]
+        basespdef=dex[8]
+        basespeed=dex[9]
 
 
 def setup(client):
