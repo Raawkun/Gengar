c6f986ee9be1583c135fb51ab47a069aed4f3da4
Rawkun <badexploits@gmail.com>
2026-01-04 18:48:36 +0100

    Update methods.py

--- cogs/methods.py
+++ cogs/methods.py
@@ -17,7 +17,7 @@ class Methods(commands.Cog):
                 evs = entry.value
                 evatk= evs.split("`ATK` ")[1].split("`")[0]
                 print(evatk)
-                evatk=int(evatk[:-5])
+                evatk=int(evatk[:-1])
                 evdef=evs.split("`DEF` ")[1].split("`")[0]
                 evdef=int(evdef[:-6])
                 evhp=evs.split("`HP` ")[1].split("`")[0]
