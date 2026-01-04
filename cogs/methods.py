8beaabf73fd9e1f83aa3232f7ad7dda1b18196ed
Rawkun <badexploits@gmail.com>
2026-01-04 16:22:24 +0100

    methods.py aktualisieren

--- cogs/methods.py
+++ cogs/methods.py
@@ -1,6 +1,7 @@
 import disnake
 from disnake.ext import commands
-import math
+import math, sqlite3
+from sqlite3 import connect
 
 class Methods(commands.Cog):
 
@@ -8,3 +9,19 @@ class Methods(commands.Cog):
         self.client = client
         self.db = connect("database.db")
 
+    async def iv_calc(self, message):
+        embed = message.embeds[0]
+        level = 
+        atk = 
+        def = 
+        hp = 
+        spatk = 
+        spdef = 
+        speed = 
+        image = embed.image.url()
+        dex = 
+        
+
+
+def setup(client):
+    client.add_cog(Modules(client))
