53014e029cb6723538b1e2c1ffef5bf33384aaf3
Rawkun <badexploits@gmail.com>
2026-05-27 11:08:11 +0200

    johto_quests.py aktualisieren

--- utility/johto/johto_quests.py
+++ utility/johto/johto_quests.py
@@ -393,12 +393,14 @@ class QuestsOfJohto(commands.Cog):
         db_mons = db_mons.fetchone()
         data = self.db.execute(f"SELECT * FROM Dex WHERE Img_Url = '{image}'")
         data = data.fetchone()
+        print(f"checking for johto legendary: {data[0]} {data[1]}")
         if data[11] == 1: #If shiny, convert to non-shiny version
             name = data[1].split("Shiny ")[1]
             data = self.db.execute(f"SELECT * FROM Dex WHERE Name = '{name}'")
             data = data.fetchone()
         #print(data[0])
         if data[0] not in mon_ids: #Check if the mon is one of the legendaries
+            print(f{data[1]} is not a johto legendary")
             return
         else: #Its a Johto Legendary, so lets check further
             print("Secret_2 Spawn!")
