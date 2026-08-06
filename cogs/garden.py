0b1b145d7b48fdc9ed45791eefdae9ece42eb865
Rawkun <badexploits@gmail.com>
2026-08-06 21:29:47 +0200

    Update garden.py

--- cogs/garden.py
+++ cogs/garden.py
@@ -2,7 +2,7 @@ import disnake
 from disnake.ext import commands
 from sqlite3 import connect
 from datetime import datetime, timedelta
-import asyncio
+import asyncio, re
 from utility.info_dict import rem_emotes, emote_list, embed_color
 from utility.id_lists import berry_times
 from cogs.reminder import Reminders
@@ -19,11 +19,11 @@ class Garden(commands.Cog):
         #DB STYLE:
         #USER_ID, Can, Slot_1, timestamp, Slot_3, Slot_4, Slot_5, Slot_6 {berry, hourly, next_stage}
 
-    async def garden_ping(self, message, userid, type, stamp, slot):
-        numbers = {"1":"one", "2":"two","3":"three","4":"four","5":"five","6":"six"}
-        if slot != "all":
-            og_slot=slot
-            slot = numbers[slot]
+    async def garden_ping(self, message, userid, check):
+        slot = check[2]
+        berry = check[4]
+        timestamp = int(check[3])
+        can = check[1]
         data = self.db.execute(f"SELECT ToggleGarden, Ping, Emote FROM Toggle WHERE User_ID = {userid}")
         data = data.fetchone()
         if data[0] == 0:
@@ -36,74 +36,61 @@ class Garden(commands.Cog):
                     desc = f"<@{userid}> - 🍓 :{slot}: ✅"
             else:
                 if type == "water":
-                    desc = f"<@{userid}> - your berry at garden slot {slot} needs water!"
+                    desc = f"<@{userid}> - your {berry} Berry at garden slot {slot} needs water!"
                 else:
-                    desc = f"<@{userid}> - your berry at garden slot {slot} is ready to be harvested!"
-            await asyncio.sleep(int(self.current_time.timestamp())-stamp)
+                    desc = f"<@{userid}> - your {berry} Berry at garden slot {slot} is ready to be harvested!"
+            await asyncio.sleep(int(self.current_time.timestamp())-timestamp)
             if data[1] == 0:
                 await message.channel.send(desc, allowed_mentions=disnake.AllowedMentions(users=False))
             else:
                 await message.channel.send(desc)
-            if slot == "all":
-                self.db.execute(f"UPDATE Garden SET Slot_1 = None, timestamp = None WHERE User_ID = {userid}")
-                self.db.commit()
-            else:
-                self.db.execute(f"UPDATE Garden SET Slot_1 = None, timestamp = None WHERE User_ID = {userid}")
-                self.db.commit()
 
-    async def garden_reminder(self, userid, type, slot, stamp,message):
-        slots = self.db.execute(f"SELECT * FROM Garden WHERE User_ID = {userid}")
-        slots = slots.fetchone()
-        print(slots)
-        slots = slots.pop(0).pop(0)
-        timers = []
-        for entry in slots:
-            if entry is not None:
-                timers.append(int(entry.split(":")[3]))
-        timers = sorted(timers)
-        if max(timers)-min(timers) <=300:
-            slot = "all"
-            print("All for one, make the timer only once")
-            asyncio.create_task(Reminders.create_tracked_task(self, Garden.garden_ping(Garden,message,userid,type,stamp,slot)))
-        else:
-            for t in timers:
-                print(t)
-                asyncio.create_task(Reminders.create_tracked_task(self, Garden.garden_ping(Garden,message,userid,type,stamp,slot)))
+            self.db.execute(f"UPDATE Garden SET Slot = None, timestamp = None, Berry = None WHERE User_ID = {userid}")
+            self.db.commit()
+
+    async def garden_reminder(self, userid, message):
+        check = self.db.execute(f"SELECT * FROM Garden WHERE User_ID = {userid}")
+        check = check.fetchone()
+        if check != None:
+            asyncio.create_task(Reminders.create_tracked_task(self, Garden.garden_ping(Garden,message,userid, check)))
+        
 
 
-    def garden_check(self, userid, entry, message):
-        slot = entry.split("**")[0]
-        stamp = entry.split("<t:")[1].split(":R>")[0]
-        stage = entry.split("STAGE ")[1].split("/")[0]
-        planted = entry.split("> ")[1].split(" Berry")[0]
-        rate = berry_times[planted]
+    def garden_check(self, userid, desc, message):
         check = self.db.execute(f"SELECT * FROM Garden WHERE User_ID = {userid}")
         check = check.fetchone()
         reply = ""
-        if check == None:
-            reply = f"What a nice garden you have there! Would you mind telling me which watering can you use?\nPlease use ``mcan wailmer/lotad/psyduck``so I can determine when to ping you for watering/harvesting!"
-            return reply
+        pots = desc.split("\n**Stage ")
+        growing = {}
+        for entry in pots:
+            if "Next stage" in entry:
+                slot = entry.split("**")[0]
+                name = entry.split("> ")[1].split(" Berry")[0]
+                stagetime = berry_times[name]
+                current_stage = entry.split("STAGE ")[1].split("/")[0]
+                n_stamp = entry.split("t:")[1].split(":")[0]
+                finishtime = int(n_stamp)+(4-(current_stage)*(stagetime*60*60))
+                growing[slot] = {"name":name,"finish":finishtime}
+
+        try:
+            best_key = min(growing, key=lambda k: growing[k]["finish"])
+            best_entry = growing[best_key]
+
+        except Exception as e:
+            print(f"There was an error: {e}")
+
+        if check[3] >= best_entry:
+            return
         else:
-            cslot = check[int(slot)+1]
-            if cslot is None:
-                cslot = f"{planted}:{rate}:{stamp}"
-                self.db.execute(f"UPDATE Garden SET Slot_{slot} = '{cslot}' WHERE User_ID = {userid}")
-                self.db.commit()
-                return reply
+            self.db.execute(f"UPDATE Garden SET Slot = {best_key}, timestamp = {best_entry}, Berry = {growing[best_key]["name"]} WHERE User_ID = {userid}")
+            self.db.commit()
+            if check[1] == "psyduck":
+                type = "harvest"
             else:
-                old_stamp = cslot.split(":")[2]
-                berry = cslot.split(":")[0]
-                if old_stamp != stamp:
-                    cslot = f"{planted}:{rate}:{stamp}"
-                    self.db.execute(f"UPDATE Garden SET Slot_{slot} = '{cslot}' WHERE User_ID = {userid}")
-                    self.db.commit()
-                    if check[1] == "psyduck":
-                        type = "harvest"
-                    else:
-                        type = "water"
-                    asyncio.create_task(Garden.garden_reminder(self,userid,type,slot,stamp,message))
-                    reply += f"Added a reminder for slot {slot}\n"
-                    return reply
+                type = "water"
+            asyncio.create_task(Garden.garden_reminder(self,userid,message))
+            reply += f"Added a reminder for slot {slot}\n"
+            return reply
 
         
     def harvest_check(self, slot):
@@ -116,34 +103,39 @@ class Garden(commands.Cog):
                         
                         
     async def user_check(self, userid, message):
-        check = self.db.execute(f"SELECT ToggleGarden, Emotes, Ping FROM Toggle WHERE User_ID = {userid}")
+        check = self.db.execute(f"SELECT ToggleGarden FROM Toggle WHERE User_ID = {userid}")
         check = check.fetchone()
         if (check == None) or (check[0] == 0):
             return
         else:
-            emb = message.embeds[0]
-            desc = emb.description
-            if "Tip:" in desc:
-                desc = desc.split("Tip:")[1]
-            pots = desc.split("**Slot ")
-            print(pots)
-            reply = ""
-            commands = f"Suggested commands are:\n"
-            for entry in pots:
-                slot = entry.split("**")[0]
-                print(entry)
-                if"Ready to harvest" in entry:
-                    commands += Garden.harvest_check(self, slot, message)
-                elif "Needs watering" in entry:
-                    commands += Garden.harvest_check(self, slot, message)
-                elif "Next stage" in entry:
-                    reply += Garden.garden_check(self, userid, entry, message)
-                elif ":lock" in entry:
-                    pass
-                else:
-                    continue
-            reply += "\n"+commands
-            await message.reply(reply)
+            check = self.db.execute(f"SELECT * FROM Garden WHERE User_ID == {userid}")
+            check = check.fetchone()
+            if check == None:
+                msg = f"What a nice garden you have there! Would you mind telling me which watering can you use?\nPlease use ``mcan wailmer/lotad/psyduck``so I can determine when to ping you for watering/harvesting!"
+                await message.reply(msg)
+                exit
+            else:
+                emb = message.embeds[0]
+                desc = emb.description
+                if "Tip:" in desc:
+                    desc = desc.split("Tip:")[1]
+                target_word = "Next stage"
+                matches = [s for s in desc if re.search(rf'{re.escape(target_word)}\b',s,re.IGNORECASE)]
+                print(matches)
+                if matches != None:
+                    reply = ""
+                    commands = f"Suggested commands are:\n"
+                    growing = {}
+                    reply += Garden.garden_check(self, userid, desc, message)
+
+                            
+                        
+
+                    
+
+                    
+                reply += "\n"+commands
+                await message.reply(reply)
 
 def setup(client):
     client.add_cog(Garden(client))
