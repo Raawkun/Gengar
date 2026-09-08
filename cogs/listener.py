86f8bcd0f2467d5f0930dcfd1edceb4cb59832e8
Rawkun <badexploits@gmail.com>
2026-09-09 01:24:18 +0200

    listener.py aktualisieren

--- cogs/listener.py
+++ cogs/listener.py
@@ -1033,7 +1033,7 @@ class Listener(commands.Cog):
                                     name = entry.split(" ")[1]
                                     id = entry.split(":")[1]
                                     if len(id) == 3 and "shiny" in name.lower():
-                                        id = str(int(id)+1000)
+                                        id = "1"+id
                                     names.append(id)
                                 names = ", ".join(names)
                                 self.db.execute(f"UPDATE Meow_Temps SET Exclusives = '{names}'")
