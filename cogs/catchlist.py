9cb4387a11b5841b28497d1d364fe6149d6c274f
Rawkun <badexploits@gmail.com>
2026-09-09 17:53:26 +0200

    catchlist.py aktualisieren

--- cogs/catchlist.py
+++ cogs/catchlist.py
@@ -45,15 +45,8 @@ class Catchlist(commands.Cog):
         if catchy:
             print(catchy)
             if catchy[2] == method:
-                caught = self.db.execute (f"SELECT Mon_ID FROM User_Catchlist WHERE User_ID = {userid}")
-                caught = caught.fetchone()
-                if caught:
-                    if str(catchy) in caught[0]:
-                        return
-                    else:
-                        await message.reply(f"Monthly Catchlist: {data[1]} with **{catchy[1]}**")
-                else:
-                    await message.reply(f"Monthly Catchlist: {data[1]} with **{catchy[1]}**")
+                await message.reply(f"Monthly Catchlist: {data[1]} with **{catchy[1]}**")
+                
         
  
             
