efa1465ef0be76256a1c15a91536fff726f52d1e
Rawkun <badexploits@gmail.com>
2026-09-09 18:33:16 +0200

    on_edit.py aktualisieren

--- cogs/on_edit.py
+++ cogs/on_edit.py
@@ -92,7 +92,7 @@ class On_Edit(commands.Cog):
                             
                             await asyncio.create_task(Catchlist.check_catchlist(self, after, sender.id, ";fish"))
                     if _embed.footer.text:
-                        if "Sort: Activity (" in _embed.footer.text and "Catchlist " in _embed.title:
+                        if "Sort: Activity (" in _embed.footer.text and "catchlist " in _embed.title.lower():
                             await asyncio.create_task(Catchlist.update_catchlist(self, after))
                         if "pokemon roll" in _embed.footer.text.lower():
                             #print("There's been a roll")
