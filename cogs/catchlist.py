eae13130d7a9dfdb5344db705fac34bae71878c9
Rawkun <badexploits@gmail.com>
2026-09-09 06:45:11 +0200

    catchlist.py aktualisieren

--- cogs/catchlist.py
+++ cogs/catchlist.py
@@ -1,1 +1,19 @@
-#new catchlist
+import disnake, asyncio
+from disnake.ext import commands
+import sqlite3, math
+from sqlite3 import connect
+
+
+class Catchlist(commands.Cog):
+
+
+    def __init__(self, client):
+        self.client = client
+        self.db = connect("database.db")
+    
+    
+
+
+
+def setup(client):
+    client.add_cog(Catchlist(client))
