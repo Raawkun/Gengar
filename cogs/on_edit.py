4c6352120ded745e217344930d9451c088c5b16a
Rawkun <badexploits@gmail.com>
2026-09-09 16:01:49 +0200

    on_edit.py aktualisieren

--- cogs/on_edit.py
+++ cogs/on_edit.py
@@ -90,7 +90,7 @@ class On_Edit(commands.Cog):
                             elif data[12]:
                                 await before.channel.send("Watch out! This one is a <:gold:1165319370801692786> Pokémon!")
                             
-                            await asyncio.create_task(Catchlist.check_catchlist(self, message, sender.id, ";f"))
+                            await asyncio.create_task(Catchlist.check_catchlist(self, after, sender.id, ";f"))
                     if _embed.footer.text:
                         if "Sort: Activity (;p, ;f, ;sz)" in _embed.footer.text and "Catchlist " in _embed.title:
                             await asyncio.create_task(Catchlist.update_catchlist(self, after))
