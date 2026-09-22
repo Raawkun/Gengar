30970ba40c21444f941e9616bcbdc65d0c502b30
Rawkun <badexploits@gmail.com>
2026-09-22 10:32:53 +0200

    listener.py aktualisieren

--- cogs/listener.py
+++ cogs/listener.py
@@ -1310,23 +1310,31 @@ class Listener(commands.Cog):
                                         type2_semi = type2.split(":")[1]
                                         #print(type2_semi)
                                     except: type2_semi = None
-                                if field.name == "Base Attack":
-                                    b_atk = field.value.split()[1]
+                                if field.name == "Atk Def HP":
+                                    b_atk = field.value.split(" ")[1]
+                                    b_def = field.value.split(" ")[3]
+                                    b_hp = field.value.split(" ")[5]
+                                #if field.name == "Base Attack":
+                                    #b_atk = field.value.split()[1]
                                     #print(b_atk)
-                                if field.name == "Base Defense":
-                                    b_def = field.value.split()[1]
+                                #if field.name == "Base Defense":
+                                    #b_def = field.value.split()[1]
                                     #print(b_def)
-                                if field.name == "Base HP":
-                                    b_hp = field.value.split()[1]
+                                #if field.name == "Base HP":
+                                    #b_hp = field.value.split()[1]
                                     #print(b_hp)
-                                if field.name == "Base Sp. Atk":
-                                    b_spatk = field.value.split()[1]
+                                if field.name == "SpA SpD SPE":
+                                    b_spatk = field.value.split(" ")[1]
+                                    b_spdef = field.value.split(" ")[3]
+                                    b_spd = field.value.split(" ")[5]
+                                #if field.name == "Base Sp. Atk":
+                                    #b_spatk = field.value.split()[1]
                                     #print(b_spatk)
-                                if field.name == "Base Sp. Def":
-                                    b_spdef = field.value.split()[1]
+                                #if field.name == "Base Sp. Def":
+                                    #b_spdef = field.value.split()[1]
                                     #print(b_spdef)
-                                if field.name == "Base Speed":
-                                    b_spd = field.value.split()[1]
+                                #if field.name == "Base Speed":
+                                    #b_spd = field.value.split()[1]
                                     #print(b_spd)
                                 if field.name == "Rarity":
                                     rarity = field.value.split(":")[1]
